fd4071d6c9a1ea9e73e7e358bd2c4c382378d1fc
PawelZabinski <79343531+PawelZabinski@users.noreply.github.com>
2021-04-10 23:40:48 +0100

    Create TicTacToe.h

---
+++ TicTacToe.h
@@ -0,0 +1,34 @@
+#pragma once
+
+enum class PlayerRotation : unsigned char
+{
+  X = 'X', O = 'O'
+};
+
+struct TicTacToeSquare
+{
+  unsigned short column, row;
+};
+
+class TicTacToe
+{
+private:
+  char m_Board[3][3];
+  PlayerRotation m_currentPlayer;
+public:
+
+  bool GameOver;
+
+  TicTacToe();
+
+  void displayBoard();
+  inline void updateBoard(TicTacToeSquare);
+
+  void getInput();
+  bool isPositionOccupied(TicTacToeSquare);
+
+  inline void changePlayer();
+
+  void checkGameStatus();
+  void playerWon(PlayerRotation player);
+};
